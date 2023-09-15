e345f0569b9f95020179b7cb40c60ea5e4b80908
sim0511 <simrandeep2012@gmail.com>
2023-09-14 17:06:23 -0700

    Magazine

---
+++ script/script.js
@@ -0,0 +1,18 @@
+function toggleSearch() {
+  let searchInput = document.querySelector('.search-input');
+  searchInput.style.display = (searchInput.style.display === 'none') ? 'inline-block' : 'none';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
